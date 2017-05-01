b24b9f99351cc0f60780367caffbdd3c9802895c
byunghyunpark <openmind8735@gmail.com>
2017-05-01 12:43:55 +0900

    템플릿을 이용해서 렌더링하는 것을 테스스

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,5 +1,6 @@
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 from django.test import TestCase
 from lists.views import home_page
 
@@ -12,7 +13,13 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn(b'<title>To-Do lists</title>', response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
+        # self.assertTrue(response.content.startswith(b'<html>'))
+        # self.assertIn(b'<title>To-Do lists</title>', response.content)
+        # self.assertTrue(response.content.strip().endswith(b'</html>'))
+
+        # 상수 테스트를 하지 않고 템플릿을 이용해서 렌더링하는 것을 테스트하도록 수정
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
+
+
 
