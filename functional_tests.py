b3aa153fa7793427ed222daf2d46ae9143dbabcb
byunghyunpark <openmind8735@gmail.com>
2017-05-05 17:01:45 +0900

    post 후에 리디렉션과 템플릿에 모든 작업 아이템 표시

--- functional_tests.py
+++ functional_tests.py
@@ -32,23 +32,25 @@ class NewVisitorTest(unittest.TestCase):
         )
 
         # '공잣깃털 사기'라고 텍스트 상자에 입력한다.
-        inputbox.send_keys('공잣깃털 사기')
+        inputbox.send_keys('공작깃털 사기')
 
         # 엔터키를 치면 페이지가 갱신되고 작업 목록에 '1: 공작깃털 사기' 아이템이 추가된다.
         inputbox.send_keys(Keys.ENTER)
-        self.check_for_row_in_list_table('1: 공잣깃털 사기')
+        import time
+        time.sleep(5)
+        self.check_for_row_in_list_table('45: 공작깃털 사기')
 
         # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다
         # 다시 '공잣깃털을 이용해서 그물 만들기' 라고 입력한다
-
-        # 에디스는 매우 체계적인 사람이다
         inputbox = self.browser.find_element_by_id('id_new_item')
+        time.sleep(5)
         inputbox.send_keys('공작깃털을 이용해서 그물 만들기')
         inputbox.send_keys(Keys.ENTER)
 
         # 페이지는 다시 객신되고 두 개 아이템이 목록에 보인다
-        self.check_for_row_in_list_table('1: 공잣깃털 사기')
-        self.check_for_row_in_list_table('2: 공잣깃털을 이용해서 그물 만들기')
+        time.sleep(5)
+        self.check_for_row_in_list_table('45: 공작깃털 사기')
+        self.check_for_row_in_list_table('47: 공작깃털을 이용해서 그물 만들기')
 
         # 에디스는 사이트가 입력한 목록을 저장하고 있느지 궁금하다
         # 사이트는 그녀를 위한 특정 URL을 생성해준다
