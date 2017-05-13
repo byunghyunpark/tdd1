5063056d2430b7c89dac6a0296c6124f99ec5f9f
byunghyunpark <openmind8735@gmail.com>
2017-05-14 00:58:45 +0900

    리스트 뷰에서 모델 유효성 검증 구현

--- django_app/lists/tests/test_views.py
+++ django_app/lists/tests/test_views.py
@@ -2,6 +2,7 @@ from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.test import TestCase
+from django.utils.html import escape
 from lists.views import home_page
 from lists.models import Item, List
 
@@ -102,3 +103,14 @@ class NewListTest(TestCase):
         )
 
         self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
+
+    def test_validation_errors_end_up_on_lists_page(self):
+        list_ = List.objects.create()
+        response = self.client.post(
+            '/lists/%d/' % (list_.id,),
+            data={'item_text': ''}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'list.html')
+        expected_error = escape('빈 아이템을 등록할 수 없습니다')
+        self.assertContains(response, expected_error)
