82448e8d73b0c5f655f8c99033b02d7e80c849ae
byunghyunpark <openmind8735@gmail.com>
2017-05-13 20:30:02 +0900

    list view를 리팩터링해서 신규 아이템 post를 처리하도록 함

--- django_app/lists/tests/test_views.py
+++ django_app/lists/tests/test_views.py
@@ -80,13 +80,11 @@ class NewListTest(TestCase):
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
 
-
-class NewItemTest(TestCase):
     def test_can_save_a_POST_request_to_an_existing_list(self):
         correct_list = List.objects.create()
 
         self.client.post(
-            '/lists/%d/add_item' % (correct_list.id,),
+            '/lists/%d/' % (correct_list.id,),
             data={'item_text': '기존 목록에 신규 아이템'}
         )
 
@@ -99,7 +97,7 @@ class NewItemTest(TestCase):
         correct_list = List.objects.create()
 
         response = self.client.post(
-            '/lists/%d/add_item' % (correct_list.id,),
+            '/lists/%d/' % (correct_list.id,),
             data={'item_text': '기존 목록에 신규 아이템'}
         )
 
