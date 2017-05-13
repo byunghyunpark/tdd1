5c7379974f7f7dfce998bb8f80f2b9a871ca0920
byunghyunpark <openmind8735@gmail.com>
2017-05-13 19:11:24 +0900

    모델 검증을 위한 신규 목록 뷰 작성

--- django_app/lists/views.py
+++ django_app/lists/views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
 
@@ -18,7 +19,13 @@ def view_list(request, list_id):
 @csrf_exempt
 def new_list(request):
     list_ = List.objects.create()
-    Item.objects.create(text=request.POST['item_text'], list=list_)
+    item = Item.objects.create(text=request.POST['item_text'], list=list_)
+    try:
+        item.full_clean()
+    except ValidationError:
+        list_.delete()
+        error = "빈 아이템을 등록할 수 없습니다"
+        return render(request, 'home.html', {'error': error})
     return redirect('/lists/%d/' % (list_.id,))
 
 
